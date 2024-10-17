bb6956d7dc605d22721dac22907ae74d06e0ee21
Mindahun-Abe <mindahun77@gmail.com>
2024-10-17 18:58:43 +0300

    to solve

--- netflix-clone-practice/src/Component/Banner/Banner.js
+++ netflix-clone-practice/src/Component/Banner/Banner.js
@@ -4,41 +4,41 @@ import requests from '../../utils/requests';
 import './Banner.css';
 
 const Banner = () => {
-	const [movie, setMovie] = useState([]);
-	useEffect(()=>{
-		(async () => {
-			try{
-	const request = await axios.get(requests.fetchNetflixOriginals)
-	setMovie(request.data.results[Math.floor(Math.random()*request.data.results.length)
-	]);
-	}catch(error){
-	console.log('error', error);
-	}
-		})();
-	},[]); 
-	  function truncate(str,n){
-return str?.length>n>str.substr(0,n-1)+'...';str;
-	};
+// 	const [movie, setMovie] = useState([]);
+// 	useEffect(()=>{
+// 		(async () => {
+// 			try{
+//	const request = await axios.get(requests.fetchNetflixOriginals)
+// 	setMovie(request.data.results[Math.floor(Math.random()*request.data.results.length)
+// 	]);
+// 	}catch(error){
+// 	console.log('error', error);
+// 	}
+// 		})();
+// 	},[]); 
+// 	  function truncate(str,n){
+// return str?.length>n>str.substr(0,n-1)+'...';str;
+// 	};
 
 	return (
 		<div>
 			<div className='Banner' 
 		style={{
 			backgroundSize: 'cover',
-            backImage: 'url("https://image.tmdb.org/t/p/original${movie?backdrop_path}")',
+            // backImage: 'url("https://image.tmdb.org/t/p/original${movie?backdrop_path}")',
 			backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat'
 }}
 		>
 		<div className='Banner_contents'>
           <h1 className='Banner_title'>
-            {movie?.title || movie?.name || movie.original_name}
+            {/* {movie?.title || movie?.name || movie.original_name} */}
            </h1>
 		   <div className='banner_buttons'>
 			<button className='banner__button play'>Play</button>
             <button className='banner__button play'>My List</button>
 		   </div>
-		    <h1 className='banner_description'>{truncate(movie?.overview,150)}</h1> 
+		    {/* <h1 className='banner_description'>{truncate(movie?.overview,150)}</h1>  */}
 		</div>
 		<div className='banner_fadeButton'/>
 		</div>
